a1993b68672d3bfa9fe9e7df1cf6b5d307d4459d
Chris Chadillon <chris@chadillon.com>
2017-02-14 18:06:47 -0500

    P2

---
+++ ImageCollectionNavDemo/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ImageCollectionNavDemo
+//
+//  Created by Chris Chadillon on 2017-02-14.
+//  Copyright © 2017 Chris Chadillon. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var theImageView: UIImageView!
+}
