76672c744391d17ac36ab8c135ea67504949ea44
Chris Chadillon <chris@chadillon.com>
2017-02-14 18:24:40 -0500

    3

--- ImageCollectionNavDemo/CollectionViewController.swift
+++ ImageCollectionNavDemo/CollectionViewController.swift
@@ -12,6 +12,8 @@ private let reuseIdentifier = "Cell"
 
 class CollectionViewController: UICollectionViewController {
 
+    let theIconList = ["backpack.png","bomb.png","book.png","briefcase.png","bubble-chat.png","bubble-heart.png","camera-photo.png","camera-polaroid.png","camera-video.png","cassette-player.png","charlie-chaplin.png","chef-hat.png","clock.png","coffee-machine.png","cog.png","computer-imac.png","computer-network.png","computer-pc.png","computer-screen-1.png","computer-screen-2.png","contact-book.png","content-download.png","cooking-pot.png","crop.png","crown.png","designer-cup.png","diving-gear.png","drink-cocktail.png","drink-coffee.png","e-mail.png","envelope.png","eyedropper.png","fork-knife.png","forklift.png","fountain-pen-head-1.png","fountain-pen-head-2.png","gauge.png","globe.png","grid.png","griller.png","handbag.png","headphone.png","heart.png","home.png","hotdog.png","icecream.png","image.png","iphone.png","ipod.png","layers-image.png","layers.png","link.png","location-pin.png","location-user.png","lock-locked.png","lock-unlocked.png","magic-wand.png","magnet.png","microphone.png","monocle.png","mp3-player.png","music-note.png","notebook-1.png","notebook-2.png","paint-brush.png","paint-bucket.png","paint-palette.png","paint-roller.png","pen-fountain.png","pen-point-tip.png","pencil-ruler.png","picture-frame.png","quill-ink.png","refresh.png","remote-control-hand.png","remote-control-screen.png","script.png","server.png","settings.png","shield-cross.png","shopping-cart-add.png","shopping-cart-remove.png","skull.png","smart-device.png","smiley.png","sneakers.png","sofa.png","stamp.png","sub-woofer.png","syringe.png","target.png","teapot.png","trash-can.png","umbrella.png","user-chat.png","user.png","vector.png","windows.png","wrenches.png","write.png"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,7 +21,7 @@ class CollectionViewController: UICollectionViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
@@ -43,19 +45,19 @@ class CollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return self.theIconList.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
     
-        // Configure the cell
+        cell.theImageView.image = UIImage(named: self.theIconList[indexPath.row])
     
         return cell
     }
